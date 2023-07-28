46afa72607fdbf1a26c646d8351db131599d9d38
Vidisha5 <74805512+Vidisha5@users.noreply.github.com>
2023-07-28 13:57:34 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,10 @@
+package c;
+import java.util.Arrays;
+import java.util.List;
+public class B {
+public static void main(String[] args) {
+	List<Integer> b = Arrays.asList(1,2,3,4);
+	Integer add = b.stream().reduce(0, (a,c)->a+c);
+	System.out.println(add);
+}
+}
