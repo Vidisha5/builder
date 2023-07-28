7999ad340c1fc6a50d1396a84b34e00bc265c908
Vidisha5 <74805512+Vidisha5@users.noreply.github.com>
2023-07-28 14:08:14 +0530

    Update B.java

--- B.java
+++ B.java
@@ -3,6 +3,6 @@ import java.util.List;
 public class B {
 public static void main(String[] args) {
 	List<Integer> b = Arrays.asList(1,2,3,4);
-	System.out.println(""+b.stream().reduce(0, (a,c)->a+c));	
+	System.out.println(b.stream().reduce(0, (a,c)->a+c));	
 }
 }
